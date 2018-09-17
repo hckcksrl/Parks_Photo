7584a4b53c49ed8390efdcbfad7ad9b91e42e57d
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-09-17 19:55:09 +0900

    Footer App

---
+++ parks/frontend/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.scss";
+ const Footer = (props, context) => (
+  <footer className={styles.footer}>
+    <div className={styles.column}>
+      <span className={styles.copyright}>2018 Parks Photo</span>
+    </div>
+  </footer>
+);
+ export default Footer;
