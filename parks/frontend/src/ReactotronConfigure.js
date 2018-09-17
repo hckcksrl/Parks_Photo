6a4f445d85663e1a75b524863785874c535279a0
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-09-17 19:54:18 +0900

    Reactotron

---
+++ parks/frontend/src/ReactotronConfigure.js
@@ -0,0 +1,9 @@
+// import Reactotron from "reactotron-react-js"
+// import {reactotronRedux } from "reactotron-redux"
+
+
+// Reactotron.configure({name : "hckgram"})
+// .use(reactotronRedux())
+// .connect();
+
+// export default Reactotron;
